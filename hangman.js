72b14f9adb9eb2f588f038d91976ccb23c2d2c16
Chandler <chndlr.wllm@gmail.com>
2017-10-14 22:56:26 -0500

    letter guess functionality initialized

--- hangman.js
+++ hangman.js
@@ -1,5 +1,5 @@
-'use strict';
 var inquirer = require('inquirer');
+
 
 var questions = [
   {
@@ -12,3 +12,34 @@ var questions = [
 inquirer.prompt(questions).then(function (answers) {
   console.log(JSON.stringify(answers, null, '  '));
 });
+
+
+inquirer.prompt({
+  type: 'input',
+  name: 'letter',
+  choices: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
+  message: 'Please select a letter.'
+}).then(letterGuessCallback);
+
+
+function letterGuessCallback (answers) {
+  // Create an instance of the WeatherAdmin. Remember WeatherAdmin is a constructor! Not an object.
+  var myAdmin = new WeatherAdmin(weatherSearchCallback);
+
+  if (answers.loginType === "admin") {
+    myAdmin.getData(function (searchLogText) {
+      console.log(searchLogText);
+    });
+  }
+  else {
+    inquirer.prompt([ {
+      type: 'input',
+      name: 'letter',
+      choices: ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'],
+      message: 'Please select a letter.'
+    }]).then(function (answers) {
+      console.log('Searching For ' + answers.location + '...');
+      myAdmin.newUserSearch(answers.name, answers.location, weatherSearchCallback);
+    })
+  }
+}
